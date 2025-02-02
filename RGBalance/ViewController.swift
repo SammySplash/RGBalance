32caffa0eeb961fa8c2a3079b9c0d0adaa4bb348
GlebSamoilik <6321657@gmail.com>
2025-02-02 11:10:08 +0000

    Initial Commit

---
+++ RGBalance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBalance
+//
+//  Created by Samoilik Hleb on 02/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
