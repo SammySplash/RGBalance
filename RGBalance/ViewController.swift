4d70e13b7394ec081955abfbfd491e4bf8c2a078
GlebSamoilik <6321657@gmail.com>
2025-02-03 00:48:00 +0000

    Sliders' work setting for UIView

--- RGBalance/ViewController.swift
+++ RGBalance/ViewController.swift
@@ -8,9 +8,36 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
+    
+    // MARK: - IB Outlets
+    @IBOutlet private var colorChangingView: UIView!
+    
+    @IBOutlet private var redSlider: UISlider!
+    @IBOutlet private var greenSlider: UISlider!
+    @IBOutlet private var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorChangingView.layer.cornerRadius = 20
+        updateColor()
     }
+    
+    @IBAction private func sliderChanged(_ sender: UISlider) {
+        updateColor()
+    }
+    
+    private func updateColor() {
+            let redValue = CGFloat(redSlider.value)
+            let greenValue = CGFloat(greenSlider.value)
+            let blueValue = CGFloat(blueSlider.value)
+            
+            colorChangingView.backgroundColor = UIColor(
+                red: redValue,
+                green: greenValue,
+                blue: blueValue,
+                alpha: 1.0
+            )
+        }
 }
 
